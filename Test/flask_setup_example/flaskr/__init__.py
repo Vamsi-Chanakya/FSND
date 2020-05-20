4fd1405226188f303488be85b6a669eea32abf00
Vamsi-Chanakya <vamsi.chanakya@gmail.com>
2020-05-20 08:50:34 -0700

    Project 2 - Completed

--- Test/flask_setup_example/flaskr/__init__.py
+++ Test/flask_setup_example/flaskr/__init__.py
@@ -1,11 +1,25 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+from models import setup_db, Plant
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'content-type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS')
+        return response
 
     @app.route('/')
     def hello():
         return jsonify({'message': 'Hello, World!'})
+
+    @app.route('/smiley')
+    def smiley():
+        return ':-)'
 
     return app
